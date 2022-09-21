e77a6b1e740de7148c1eaf677ecbca94d8d8f849
Sankara_Moorthi <107635975+SankaraMoothi@users.noreply.github.com>
2022-09-21 20:06:36 +0530

    Add files via upload

---
+++ COVIT-19/script.js
@@ -0,0 +1,111 @@
+/* Creating a div element and setting its class attribute to container. */
+
+let container=document.createElement("div");
+container.setAttribute("class","container");
+
+
+/* Creating a div element and setting its class attribute to main and id attribute to main. */
+
+let div1=document.createElement("div");
+div1.setAttribute("class","main");
+div1.setAttribute("id","main")
+
+
+/* Creating a h1 element and setting its class attribute to Mainheading and innerHTML to Covid-19. */
+
+let h1= document.createElement("h1");
+h1.setAttribute("class","Mainheading")
+h1.innerHTML="Covid-19";
+
+/* Creating a paragraph element and setting its id attribute to discription and innerHTML to Its About
+Covid-19 Cases. */
+
+let pra=document.createElement("p");
+pra.setAttribute("id","discription")
+pra.innerHTML="Its About Covid-19 Cases";
+
+/* Creating a div element and setting its id attribute to main2. */
+
+let div2=document.createElement("div");
+div2.setAttribute("id","main2");
+
+/* Creating a div element and setting its id attribute to main3. */
+
+let div3=document.createElement("div");
+div3.setAttribute("id","main3");
+
+/* Creating an input element and setting its type attribute to text, min-length attribute to 3, class
+attribute to form-control, id attribute to country and req attribute to required. */
+
+let input=document.createElement("input")
+input.setAttribute("type","text");
+input.setAttribute("min-length","3");
+input.setAttribute("placeholder","Enter the country name")
+input.setAttribute("class","form-control");
+input.setAttribute("id","country");
+input.setAttribute("req","required");
+
+/* Creating a button element and setting its type attribute to button, value attribute to Search, class
+attribute to btn btn-primary, id attribute to search and innerHTML to Search. */
+
+let search=document.createElement("button");
+search.setAttribute("type","button");
+search.setAttribute("value","Search")
+search.setAttribute("class","btn btn-primary");
+search.setAttribute("id","search");
+search.innerHTML="Search";
+
+/* Adding an event listener to the search button. */
+
+search.addEventListener("click",foo);
+
+
+    
+
+async function foo(){
+    let val=document.getElementById("country").value;
+    var url=`https://api.covid19api.com/dayone/country/${val}`;
+    
+    let res= await fetch(url);
+    let res1= await res.json();
+    active.innerHTML=`Total Active Case:${res1[res1.length-1].Active}`
+    Confirmed.innerHTML=`Total Confirmed Case:${res1[res1.length-1].Confirmed}`
+    recovered.innerHTML=`Total Recovered Case:${res1[res1.length-1].Recovered}`
+    dead.innerHTML=`Total Death Case:${res1[res1.length-1].Deaths}`
+}
+
+/* Creating a div element and setting its id attribute to flag. */
+
+let flag=document.createElement("div");
+flag.setAttribute("id","flag")
+
+/* Creating a div element and setting its id attribute to active. */
+
+let active=document.createElement("div")
+active.setAttribute("id","active");
+
+/* Creating a div element and setting its id attribute to confirmed. */
+
+let Confirmed=document.createElement("div")
+Confirmed.setAttribute("id","confirmed");
+
+/* Creating a div element and setting its id attribute to recovered. */
+
+let recovered=document.createElement("div")
+recovered.setAttribute("id","recovered")
+
+/* Creating a div element and setting its id attribute to dead. */
+
+let dead=document.createElement("div")
+dead.setAttribute("id","dead");
+
+/* Appending the elements to the respective divs. */
+
+div1.append(h1,pra);
+div2.append(input,search)
+div3.append(flag,active,Confirmed,recovered,dead)
+container.append(div1,div2,div3);
+document.body.append(container);
+
+
+
